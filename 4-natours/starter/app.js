b102918aee3eb588577ca2711bfb1149a99b76d8
u210709028 <smhargn@gmail.com>
2025-02-26 00:10:24 +0300

    initial commit

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -9,15 +10,19 @@ const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // 1 ) MIDDLEWARES
 //console.log(process.env.NODE_ENV);
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP headers
 app.use(helmet());
 
@@ -62,7 +67,8 @@ app.use(hpp({
 }));
 
 
-app.use(express.static(`${__dirname}/public`));
+//app.use(express.static(`${__dirname}/public`));
+
 
 
 // app.use((req,res,next) => {
@@ -88,7 +94,7 @@ app.use((req,res,next) => {
 // 2 ) Handlers
 
 
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ 4-natours/starter/routes/viewRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const viewController = require('../controllers/viewsController');
+
+
+const router = express.Router();
+
+// router.get('/', (req, res) => {
+//     res.status(200).render('base', {
+//         tour : 'The Forest Hiker',
+//         user : 'Jonas'
+//     });
+// })
+
+router.get('/',viewController.getOverview)
+router.get('/tour', viewController.getTour);
+
+
+module.exports = router;
